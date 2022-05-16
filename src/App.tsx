70f2fb3517c3b7f2ac7caed885266c7fe51f8b40
Wellington R <sousa_wellington@outlook.com>
2022-05-16 17:39:29 -0300

    🔨 Refactor: Change main files for .ts and configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-
-import Routes from './routes';
-
+import Routes from './routes/index';
 import GlobalStyle from './styles/global';
 
+
 const App = () => (
-  <>
+  <div>
     <GlobalStyle />
     <Router>
       <Routes />
     </Router>
-  </>
+  </div>
 );
 
 export default App;
